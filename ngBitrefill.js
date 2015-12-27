6fac1b411ac8d6d408896ff8799237350b2f9770
Kosta Korenkov <7r0ggy@gmail.com>
2015-12-28 01:31:03 +0300

    Fix lookupNumber, simplify code

--- ngBitrefill.js
+++ ngBitrefill.js
@@ -3,121 +3,105 @@
 angular.module('ngBitrefill', ['base64'])
   .provider('bitrefill', function() {
     var apiKey,
-        apiPass;
+        apiSecret;
         
-    this.setCredentials = function(key, password) {
+    this.setCredentials = function(key, secret) {
       this.apiKey = key;
-      this.apiPass = password;
+      this.apiSecret = secret;
     }
-    
-    function Bitrefill(cfg, $log, $http, $base64) {
-      if (!(this instanceof Bitrefill))
-        return new Bitrefill(cfg)
-
-      this.cfg = cfg
-      this.$log = $log;
-      this.$http = $http;
-      this.$base64 = $base64;
-
-      /*assert(this.cfg, 'cfg is required')
-      assert(this.cfg.key, 'cfg.key is required')
-      assert(this.cfg.secret, 'cfg.secret is required')
-      assert(this.cfg.url, 'cfg.url is required')*/
-      this.authurl = "https://" + this.cfg.key + ":" + this.cfg.secret + "@" + this.cfg.url;
-    }
-    
-    Bitrefill.prototype.handleDataResponse = function(response, cb) {
-      var data = JSON.parse(response.data);
-      if (data.error) {
-        cb(data.error);
-      } else if (data.errorMessage) {
-        cb(data.errorMessage);
-      } else {
-        cb(null, data);
-      }
-    };
-    
-    Bitrefill.prototype.handleErrorResponse = function(response, cb) {
-      this.$log.error(response.status + ': ' + JSON.stringify(response.data));
-      cb(response.data);
-    };
-    
-    Bitrefill.prototype.request = function(config, cb) {
-      var self = this;
-      config.headers = {
-        Authorization: 'Basic ' + this.$base64.encode(this.cfg.key + ':' + this.cfg.secret)
-      };
-      this.$http(config).then(function successCallback(response) {
-        self.handleDataResponse(response, cb);
-      }, function errorCallback(response) {
-        self.handleErrorResponse(response, cb);
-      });
-
-    }
-
-    Bitrefill.prototype.inventory = function(cb) {
-      var params = {
-        method: 'GET',
-        url: this.authurl + "/inventory/"
-      };
-      
-      this.request(params, cb);
-    };
-
-    Bitrefill.prototype.lookupNumber = function(number, operator, cb) {
-      if (typeof operator == 'function') {
-        operator = null;
-        cb = operator;
-      }
-      var params = {
-        method: 'GET',
-        url: this.authurl + "/lookup_number",
-        data: {
-          number: number,
-          operatorSlug: operator || undefined
-        }
-      };
-      
-      this.request(params, cb);
-    };
-
-    Bitrefill.prototype.placeOrder = function(number, operator, pack, email, cb) {
-      var params = {
-        method: "POST",
-        url: this.authurl + "/order",
-        data: {
-          number: number,
-          valuePackage: pack,
-          operatorSlug: operator,
-          email: email
-        }
-      };
-      
-      this.request(params, cb);
-    };
-
-    Bitrefill.prototype.orderStatus = function(order_id, cb) {
-      var params = {
-        method: "GET",
-        url: this.authurl + "/order/" + order_id
-      };
-      
-      this.request(params, cb);
-    };
-
     
     this.$get = function($log, $http, $base64) {
-      if (!this.apiKey || !this.apiPass) {
-        throw new Error("BitrefillService is not configured. " +
-                            "Call setCredentials(apiKey, apiPass) on it's provider");
+      var that = this;
+      if (!that.apiKey || !that.apiSecret) {
+        throw new Error("Bitrefill service is not configured. " +
+                        "Call setCredentials(apiKey, apiSecret) on it's provider");
       }
       
-      var bitrefill = new Bitrefill({
-        key: this.apiKey,
-        secret: this.apiPass,
-        url: 'api.bitrefill.com/v1'
-      }, $log, $http, $base64);
+      function Bitrefill() {
+        this.baseUrl = "https://api.bitrefill.com/v1";
+      }
       
-      return bitrefill;
+      Bitrefill.prototype.handleDataResponse = function(response, cb) {
+        var data = response.data;
+        if (data.error) {
+          cb(data.error);
+        } else if (data.errorMessage) {
+          cb(data.errorMessage);
+        } else {
+          cb(null, data);
+        }
+      };
+      
+      Bitrefill.prototype.handleErrorResponse = function(response, cb) {
+        $log.error(response.status + ': ' + JSON.stringify(response.data));
+        cb(response.status == 500 ? 'Server error' : response.data);
+      };
+      
+      Bitrefill.prototype.request = function(config, cb) {
+        var self = this;
+        config.headers = {
+          Authorization: 'Basic ' + $base64.encode(that.apiKey + ':' + that.apiSecret)
+        };
+        config.url = this.baseUrl + config.url;
+        $http(config).then(function successCallback(response) {
+          self.handleDataResponse(response, cb);
+        }, function errorCallback(response) {
+          self.handleErrorResponse(response, cb);
+        });
+
+      }
+
+      Bitrefill.prototype.inventory = function(cb) {
+        var params = {
+          method: 'GET',
+          url: this.authurl + "/inventory/"
+        };
+        
+        this.request(params, cb);
+      };
+
+      Bitrefill.prototype.lookupNumber = function(number, operator, cb) {
+        if (typeof operator == 'function') {
+          operator = null;
+          cb = operator;
+        }
+        var params = {
+          method: 'GET',
+          url: "/lookup_number",
+          params: {
+            number: number,
+            operatorSlug: operator || undefined
+          }
+        };
+        
+        this.request(params, cb);
+      };
+
+      Bitrefill.prototype.placeOrder = function(number, operator, pack, email, refundAddress, cb) {
+        var params = {
+          method: "POST",
+          url: "/order",
+          data: {
+            number: number,
+            valuePackage: pack,
+            operatorSlug: operator,
+            email: email,
+            refund_btc_address: refundAddress
+          }
+        };
+        
+        this.request(params, cb);
+      };
+
+      Bitrefill.prototype.orderStatus = function(order_id, cb) {
+        var params = {
+          method: "GET",
+          url: "/order/" + order_id
+        };
+        
+        this.request(params, cb);
+      };
+
+      return new Bitrefill();
     };
 });
